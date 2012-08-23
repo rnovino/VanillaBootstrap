7dacd99442d1f6437dea32787d90687908e8b77c
Kasper Isager <kasperisager@gmail.com>
2012-08-23 12:26:01 +0200

    Patch up name

--- about.php
+++ about.php
@@ -1,6 +1,6 @@
 <?php if (!defined('APPLICATION')) exit();
-$ThemeInfo['Bootstrap'] = array(
-   'Name' => 'Bootstrap',
+$ThemeInfo['VanillaBootstrap'] = array(
+   'Name' => 'VanillaBootstrap',
    'Description' => "Based on Bootstrap from Twitter and inspired by LiveRoom.<br>Be sure to read the documentation found here: <a href='https://github.com/kasperisager/VanillaBootstrap'>https://github.com/kasperisager/VanillaBootstrap</a>",
    'Version' => '1.1b-2.0',
    'Author' => "Kasper K. Isager",
